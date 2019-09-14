afff88aa6e6e7d061fd210784b96114ad2294e1f
Wandenberg <wandenberg@gmail.com>
2019-09-14 17:33:04 +0200

    example repository

--- example/site/hello_nexus/manifests/init.pp
+++ example/site/hello_nexus/manifests/init.pp
@@ -1,4 +1,15 @@
 class hello_nexus (
 ) {
   notify { 'Add here the resources to test on Nexus3': }
+  nexus3_repository { 'jcenter':
+    ensure                         => 'present',
+    layout_policy                  => 'strict',
+    online                         => 'true',
+    provider_type                  => 'maven2',
+    remote_auth_type               => 'none',
+    remote_url                     => 'https://jcenter.bintray.com',
+    strict_content_type_validation => 'true',
+    type                           => 'proxy',
+    version_policy                 => 'release',
+  }
 }
